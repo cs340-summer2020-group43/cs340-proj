159429fa739f48050a899d6eb217b8024613f70f
Nick Barber <nickwbarber@gmail.com>
2020-07-19 17:44:33 -0500

    Implement basic webserver

---
+++ src/app/app.go
@@ -0,0 +1,36 @@
+package main
+
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+
+const (
+	port int = 3000
+	urlBasePath = "/cs340"
+	templatesBasePath = "../templates"
+)
+
+
+func main() {
+
+	// The main page
+	http.HandleFunc(
+		urlBasePath + "/",
+		func(writer http.ResponseWriter, req *http.Request) {
+			data := make(map[string]string)
+			data["Title"] = "Test Page"
+			data["Body"] = "<h1>It works!</h1>"
+
+			var t *template.Template
+			t = template.Must(t.ParseFiles(templatesBasePath + "/base.html"))
+			t.Execute(writer, data)
+	})
+
+
+	fmt.Printf("Server started on port %v\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+}
