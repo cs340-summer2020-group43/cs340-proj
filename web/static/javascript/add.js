e90fa5347c4315c472da4a82fe98f466d0b26da2
Jennifer <jennifer_d777@yahoo.com>
2020-08-10 12:11:31 -0700

    #6 user-add.html input completed and tester-add.html file edited

--- web/static/javascript/add.js
+++ web/static/javascript/add.js
@@ -5,24 +5,29 @@ myForm.addEventListener('submit', (e) => {
     //var userNameValue = document.getElementById('user_add_input_name').value;
     let formData = new FormData(myForm);
     let searchParam = new URLSearchParams();
+    searchParam.append('table', 'Users');
+    
+
 
     for (var pair of formData) {
         searchParam.append(pair[0], pair[1]);
+        //searchParam.append(pair[0], pair[1]);
+        //searchParam.append(pair[0], pair[1]);
+        console.log(pair[0], pair[1])
     };
-
-   fetch('https://postman-echo.com/post', {
+    fetch('/cs340/insert', {
         method: "POST",
         //headers: {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-        cache: 'no-cache',
+        //cache: 'no-cache',
         headers: {
             'Content-type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Client-ID [my-client-id]'
+            //'Authorization': 'Client-ID [my-client-id]'
         },
         body: searchParam
         })
         .then((response) => {
             if (!response.ok) {
-                 throw Error(response.statusText);   
+                    throw Error(response.statusText);   
             }
             console.log(response);
             return response.text();
