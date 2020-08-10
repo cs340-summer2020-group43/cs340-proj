9c30292e06599fd804052c4214afe6e330305c66
Nick Barber <nickwbarber@gmail.com>
2020-08-10 16:05:57 -0500

    Clean up old code, Improve readability
    - Removes some old code that had been commented out.
    - Inserted/removed newlines as appropriate for readability

--- web/static/javascript/add.js
+++ web/static/javascript/add.js
@@ -2,26 +2,20 @@ const myForm = document.getElementById('userAddForm');
 
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    //var userNameValue = document.getElementById('user_add_input_name').value;
+
     let formData = new FormData(myForm);
     let searchParam = new URLSearchParams();
-    searchParam.append('table', 'Users');
-    
 
+    searchParam.append('table', 'Users');
 
     for (var pair of formData) {
         searchParam.append(pair[0], pair[1]);
-        //searchParam.append(pair[0], pair[1]);
-        //searchParam.append(pair[0], pair[1]);
-        console.log(pair[0], pair[1])
     };
+
     fetch('/cs340/insert', {
         method: "POST",
-        //headers: {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-        //cache: 'no-cache',
         headers: {
             'Content-type': 'application/x-www-form-urlencoded',
-            //'Authorization': 'Client-ID [my-client-id]'
         },
         body: searchParam
         })
