9d9acf793ea34927cbe4c3dac6cad090b327c869
Jennifer <jennifer_d777@yahoo.com>
2020-08-11 00:32:50 -0700

    Format Error Add Client Side errorAdd.js is working error-add.html is working

---
+++ web/static/javascript/errorAdd.js
@@ -0,0 +1,37 @@
+const myForm = document.getElementById('errorAddButton');
+
+myForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    let addForm = document.getElementById('errorAddForm');
+    let formData = new FormData(addForm);
+    let searchParam = new URLSearchParams();
+    searchParam.append('table', 'Errors');
+    console.log("check");
+    
+    for (var pair of formData) {
+        searchParam.append(pair[0], pair[1]);
+        console.log(pair[0], pair[1]);
+    };
+
+    fetch('/cs340/insert', {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Client-ID [my-client-id]'
+        },
+        body: searchParam
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);   
+            }
+            console.log(response);
+            return response.text();
+        })
+        .then((result) => {
+            console.log('success', result);
+        })
+        .catch((error) => {
+            console.error('Error', error);
+        })
+});
