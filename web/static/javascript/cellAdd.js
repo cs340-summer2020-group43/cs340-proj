202edb7431c512e71bf5ef25177c75df659c626b
Nick Barber <nickwbarber@gmail.com>
2020-08-13 15:14:03 -0500

    Fix bug where Cells could not be added from Client
    - Fixes #22

--- web/static/javascript/cellAdd.js
+++ web/static/javascript/cellAdd.js
@@ -1,37 +1,35 @@
-const myForm = document.getElementById('cellAddButton');
+const addForm = document.getElementById('cellAddForm');
 
-myForm.addEventListener('submit', (e) => {
+addForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    let addForm = document.getElementById('cellAddForm');
+
     let formData = new FormData(addForm);
-    let searchParam = new URLSearchParams();
-    searchParam.append('table', 'Cells');
-    console.log("check");
+    let searchParams = new URLSearchParams();
+
+    searchParams.append('table', 'Cells');
     
     for (var pair of formData) {
-        searchParam.append(pair[0], pair[1]);
-        console.log(pair[0], pair[1]);
+        searchParams.append(pair[0], pair[1]);
     };
 
     fetch('/cs340/insert', {
         method: "POST",
         headers: {
             'Content-type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Client-ID [my-client-id]'
         },
-        body: searchParam
-        })
-        .then((response) => {
-            if (!response.ok) {
-                throw Error(response.statusText);   
-            }
-            console.log(response);
-            return response.text();
-        })
-        .then((result) => {
-            console.log('success', result);
-        })
-        .catch((error) => {
-            console.error('Error', error);
-        })
+        body: searchParams,
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw Error(response.statusText);   
+        }
+        console.log(response);
+        return response.text();
+    })
+    .then((result) => {
+        console.log('success', result);
+    })
+    .catch((error) => {
+        console.error('Error', error);
+    })
 });
